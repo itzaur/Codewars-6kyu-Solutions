4d9b80afa216b38faac5d9f51de4d5406fa75311
itzaur <preneur85@gmail.com>
2024-09-05 22:13:13 +0300

    Add Thinking & Testing : Who are you?

---
+++ Thinking & Testing : Who are you?.js
@@ -0,0 +1,18 @@
+/*
+Description:
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+*/
+function testit(a, b) {
+  console.log('My name is Paul');
+  return a + b;
+}
+
+console.log(testit(1, 1)); //2
+console.log(testit(1, 2)); //3
+console.log(testit(2, 2)); //4
